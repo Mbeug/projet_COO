bee77ba5fa3c91b7ca40c0cb1e40e550960b4d48
CryoCardiogram <menalewa@live.com>
2016-04-27 00:19:07 +0200

    Ecran de démarrage & menu déroulant

--- UCLove/app/src/main/java/com/example/maximebeugoms/uclove/LoginActivity.java
+++ UCLove/app/src/main/java/com/example/maximebeugoms/uclove/LoginActivity.java
@@ -1,11 +1,13 @@
 package com.example.maximebeugoms.uclove;
 
-import android.app.Fragment;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 
 /**
  * Created by damien on 22/04/16.
@@ -14,9 +16,15 @@ public class LoginActivity extends MainActivity
 {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.drawer_layout, new LoginFragment())
+                    .commit();
+        }
     }
+
+
 }
 
 
--- UCLove/app/src/main/java/com/example/maximebeugoms/uclove/MainActivity.java
+++ UCLove/app/src/main/java/com/example/maximebeugoms/uclove/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.Gravity;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -52,7 +53,7 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
