afa388c94e615ee41dcb2515ba66c10b10b82bbd
Damien Detry <damien.detry@student.uclouvain.be>
2016-04-28 17:16:25 +0200

    Changes

--- UCLove/app/src/main/java/com/example/maximebeugoms/uclove/DatabaseHandler.java
+++ UCLove/app/src/main/java/com/example/maximebeugoms/uclove/DatabaseHandler.java
@@ -4,26 +4,19 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
-
-/**
- * Created by damien on 26/04/16.
- */
-
-/*
-*   Je ne suis pas sur pour les clés étrangères qui font références a d'autre table lors de la création des tables
-*/
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class DatabaseHandler extends SQLiteOpenHelper
 {
     // Pour la table Profil
-    public static final String PROFIL_ID = "id_profil";
     public static final String PROFIL_NOM = "nom";
     public static final String PROFIL_SEXE = "sexe";
     public static final String PROFIL_AGE = "age";
     public static final String PROFIL_COULEUR_CHEVEUX = "couleur_cheveux";
     public static final String PROFIL_ORIENTATION = "orientation";
 
-    // photo + localisation + date_dispo
 
     //Pour la table Utilisateur
 
@@ -32,38 +25,25 @@ public class DatabaseHandler extends SQLiteOpenHelper
     public static final String UTILISATEUR_MAIL = "mail";
     public static final String UTILISATEUR_PASSWORD = "password";
     public static final String UTILISATEUR_ID_PROFIL = "id_profil";
-    public static final String UTILISATEUR_ID_HISTORIQUE = "id_historique";
-    public static final String UTILISATEUR_ID_PREFERENCE = "id preference";
 
-    // Pour la table Historique
-
-    public static final String HISTORIQUE_ID_HISTORIQUE = "id_historique";
-    public static final String HISTORIQUE_ID_EVENT = "id_event";
 
     // Pour la table Evenement
 
-    public static final String EVENEMENT_ID_EVENT = "id_event";
     public static final String EVENEMENT_DATE = "date";
     public static final String EVENEMENT_TYPE = "type";
-    public static final String EVENEMENT_ID_HISTORIQUE ="id_historique";
+
 
     // Pour la table relation
 
-    public static final String RELATION_ID_RELATION = "id_relation";
     public static final String RELATION_ETAT_ACCEPTATION = "etat_acceptation";
-    public static final String RELATION_ID_UTILISATEUR1 = "id_utilisateur1";
-    public static final String RELATION_ID_UTILISATEUR2 = "id_utilisateur2";
 
     //Pour la table Message
 
-    public static final String MESSAGE_ID_MESSAGE = "id_message";
     public static final String MESSAGE_TEXTE = "texte";
     public static final String MESSAGE_DATE = "date";
-    public static final String MESSAGE_ID_RELATION ="id_relation";
 
     // Pour la table Preference
 
-    public static final String PREFERENCE_ID_PREF = "id_pref";
     public static final String PREFERENCE_LANGUE ="langue";
     public static final String PREFERENCE_NIVEAU_CONFIDENTIALITE = "niveau_confidentialite";
 
@@ -73,7 +53,6 @@ public class DatabaseHandler extends SQLiteOpenHelper
 
     public static final String PROFIL_TABLE_NAME = "Profil";
     public static final String UTILISATEUR_TABLE_NAME = "Utilisateur";
-    public static final String HISTORIQUE_TABLE_NAME = "Historique";
     public static final String EVENEMENT_TABLE_NAME = "Evenement";
     public static final String RELATION_TABLE_NAME = "Relation";
     public static final String MESSAGE_TABLE_NAME = "Message";
@@ -84,7 +63,7 @@ public class DatabaseHandler extends SQLiteOpenHelper
      */
     public static final String PROFIL_TABLE_CREATE =
             "CREATE TABLE " + PROFIL_TABLE_NAME + " (" +
-                    PROFIL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+
                     PROFIL_NOM + " TEXT, " +
                     PROFIL_SEXE + " TEXT, " +
                     PROFIL_AGE + "INTEGER, " +
@@ -93,39 +72,26 @@ public class DatabaseHandler extends SQLiteOpenHelper
 
     public static final String UTILISATEUR_TABLE_CREATE =
             "CREATE TABLE" + UTILISATEUR_TABLE_NAME + "(" +
-                    UTILISATEUR_ID_USER + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     UTILISATEUR_LOGIN + "TEXT, " +
                     UTILISATEUR_MAIL + "TEXT, " +
                     UTILISATEUR_PASSWORD + "TEXT, " +
-                    UTILISATEUR_ID_PROFIL + "INTEGER, " +
-                    UTILISATEUR_ID_HISTORIQUE + "INTEGER FOREIGN KEY REFERENCES ID_HISTORIQUE, " +
-                    UTILISATEUR_ID_PREFERENCE + "INTEGER FOREIGN KEY REFERENCES ID_PREFERENCE); ";
-
-    public static final String HISTORIQUE_TABLE_CREATE =
-            "CREATE TABLE" + HISTORIQUE_TABLE_NAME + "(" +
-                    HISTORIQUE_ID_HISTORIQUE + "INTEGER PRIMARY KEY AUTOINCREMENT," +
-                    HISTORIQUE_ID_EVENT + "INTEGER FOREIGN KEY REFERENCES ID_EVENT);";
+                    UTILISATEUR_ID_PROFIL + "INTEGER);";
 
     public static final String EVENEMENT_TABLE_CREATE =
             "CREATE TABLE" + EVENEMENT_TABLE_NAME + "(" +
-                    EVENEMENT_ID_EVENT + "INTEGER PRIMARY KEY AUTOINCREMENT," +
                     EVENEMENT_DATE + "DATE, " +
-                    EVENEMENT_TYPE + "STRING, " +
-                    EVENEMENT_ID_HISTORIQUE + "INTEGER FOREIGN KEY REFERENCES ID_HISTORIQUE );";
+                    EVENEMENT_TYPE + "STRING);";
 
     public static final String RELATION_TABLE_CREATE =
             "CREATE TABLE" + RELATION_TABLE_NAME +"(" +
                     RELATION_ID_RELATION + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                    RELATION_ETAT_ACCEPTATION + "BOOLEAN ," +
-                    RELATION_ID_UTILISATEUR1 + "INTEGER REFERENCES UTILISATEUR, " +
-                    RELATION_ID_UTILISATEUR2 + "INTEGER REFERENCES UTILISATEUR);";
+                    RELATION_ETAT_ACCEPTATION + "BOOLEAN);";
 
     public static final String MESSAGE_TABLE_CREATE =
             "CREATE TABLE" + MESSAGE_TABLE_NAME + "(" +
                     MESSAGE_ID_MESSAGE + "INTEGER PRIMARY KEY AUTOINCREMENT," +
                     MESSAGE_TEXTE + "TEXT, " +
-                    MESSAGE_DATE + "DATE ," +
-                    MESSAGE_ID_RELATION + "INTEGER FOREIGN KEY REFERENCES ID_RELATION);";
+                    MESSAGE_DATE + "DATE);";
 
     public static final String PREFERENCE_TABLE_CREATE =
             "CREATE TABLE" + PREFERENCE_TABLE_NAME + "(" +
@@ -144,7 +110,6 @@ public class DatabaseHandler extends SQLiteOpenHelper
         db.execSQL(EVENEMENT_TABLE_CREATE);
         db.execSQL(PREFERENCE_TABLE_CREATE);
         db.execSQL(RELATION_TABLE_CREATE);
-        db.execSQL(HISTORIQUE_TABLE_CREATE);
         db.execSQL(MESSAGE_TABLE_CREATE);
     }
 
@@ -153,17 +118,23 @@ public class DatabaseHandler extends SQLiteOpenHelper
     public static final String EVENEMENT_TABLE_DROP = "DROP TABLE IF EXISTS " + EVENEMENT_TABLE_NAME +";";
     public static final String PREFERENCE_TABLE_DROP = "DROP TABLE IF EXISTS" + PREFERENCE_TABLE_NAME+";";
     public static final String RELATION_TABLE_DROP = "DROP TABLE IF EXISTS" + RELATION_TABLE_NAME+";";
-    public static final String HISTORIQUE_TABLE_DROP = "DROP TABLE IF EXISTS" + HISTORIQUE_TABLE_NAME+";";
     public static final String MESSAGE_TABLE_DROP = "DROP TABLE If EXISTS" + MESSAGE_TABLE_NAME + ";";
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        try
+        {
+            BufferedReader buf = new BufferedReader(new FileReader("file.java")); // lire fichier pour mettre des data dans les tables
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
         db.execSQL(PROFIL_TABLE_DROP); //requete sql dont on ignore la réponse (execSQL())
         db.execSQL(UTILISATEUR_TABLE_DROP);
         db.execSQL(EVENEMENT_TABLE_DROP);
         db.execSQL(PREFERENCE_TABLE_DROP);
         db.execSQL(RELATION_TABLE_DROP);
-        db.execSQL(HISTORIQUE_TABLE_DROP);
         db.execSQL(MESSAGE_TABLE_DROP);
         onCreate(db);
     }
