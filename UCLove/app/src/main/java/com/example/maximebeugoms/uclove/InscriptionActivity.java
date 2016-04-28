0b5d2ff24229b85ed1b050ef7c04bd0a77573dc5
Maxime Beugoms <maxime.beugoms@student.uclouvain.be>
2016-04-28 17:14:38 +0200

    up mise a jour

---
+++ UCLove/app/src/main/java/com/example/maximebeugoms/uclove/InscriptionActivity.java
@@ -0,0 +1,13 @@
+package com.example.maximebeugoms.uclove;
+
+import android.os.Bundle;
+
+/**
+ * Created by maximebeugoms on 28/04/16.
+ */
+public class InscriptionActivity extends MainActivity {
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.inscription_view);
+        }
+}
--- UCLove/app/src/main/java/com/example/maximebeugoms/uclove/LoginActivity.java
+++ UCLove/app/src/main/java/com/example/maximebeugoms/uclove/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.maximebeugoms.uclove;
 
+import android.content.Intent;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -14,14 +16,36 @@ import android.view.ViewGroup;
  */
 public class LoginActivity extends MainActivity
 {
+    private Button connexion=null;
+    private Button inscription=null;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
+        setContentView(R.layout.login_view);
+        /*if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.drawer_layout, new LoginFragment())
                     .commit();
-        }
+        }*/
+        connexion = (Button) findViewById(R.id.btn_co);
+        inscription= (Button) findViewById(R.id.btn_insc);
+
+        connexion.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View vue){
+                Intent intent = new Intent(LoginActivity.this,ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        inscription.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View vue){
+                Intent intent = new Intent(LoginActivity.this,InscriptionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
     /**
      * Ce fragment contient le login_view
@@ -38,7 +62,6 @@ public class LoginActivity extends MainActivity
             return inflater.inflate(R.layout.login_view, container, false);
         }
     }
-
 }
 
 
