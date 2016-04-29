4d49822cf9b4b8671c6278e29d520f89f75dd1f1
vixwilli <william.salame@student.uclouvain.be>
2016-04-29 05:10:40 +0200

    Delete lignes log

--- UCLove/app/src/main/java/com/example/maximebeugoms/uclove/MainActivity.java
+++ UCLove/app/src/main/java/com/example/maximebeugoms/uclove/MainActivity.java
@@ -9,7 +9,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -80,7 +79,6 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-        Log.d("btn","Rentre");
         if (id == R.id.nav_profil) {
 
             // Handle the action
